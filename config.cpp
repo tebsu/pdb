64dc47384e144631b32e1c96b7a9f3dea190a15f
Tebin Ulrich <info@tebinulrich.de>
2016-10-07 17:05:22 +0300

    Initial commit

---
+++ config.cpp
@@ -0,0 +1,9 @@
+class CfgPatches
+{
+	class SRU_PDB
+	{
+		units[] = {"SRU_PDB"};
+		requiredVersion = 1.0;
+		requiredAddons[] = {"A3_Modules_F"};
+	};
+};
---
+++ mod.cpp
@@ -0,0 +1,6 @@
+dir = "@sru_pdb";
+picture = "SRU.paa";
+name = "SRU Persistent Database";
+actionName = "Website";
+action = "http://sru-gaming.de";
+description = "SRU PDB by tulrich & Spackenbremse";
